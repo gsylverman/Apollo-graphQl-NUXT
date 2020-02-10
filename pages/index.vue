f2daf9b1df70a3c1d289d98c913b31144bd5a70a
gavril.hasmasan <gavril.hasmasan@pitechnologies.ro>
2020-02-10 17:41:44 +0200

    Apollo gql

---
+++ pages/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="character in characters.results" :key="character.id">
+        Name: {{ character.name }} ID:{{ character.id }}
+      </li>
+    </ul>
+    <div>Ch: {{ character.name }}</div>
+  </div>
+</template>
+
+<script>
+import gql from 'graphql-tag'
+
+export default {
+  apollo: {
+    characters: gql`
+      query getCharacters {
+        characters {
+          results {
+            id
+            name
+            status
+          }
+        }
+      }
+    `,
+    character: gql`
+      query getCharacter {
+        character(id: "2") {
+          id
+          name
+        }
+      }
+    `
+  }
+}
+</script>
